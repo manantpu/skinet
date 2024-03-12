67eaf5c98df7a93068ca8915009045d18a195323
Manasi <manasi.anantpurkar@gmail.com>
2024-03-12 15:23:48 -0700

    End of API application

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Core.Entities;
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts(){
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id){
+            var product = await _context.Products.FindAsync(id);
+            return product;
+        }
+    }
+}
